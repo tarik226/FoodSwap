0abbb8ec2aa9d7a194cd98c7a42bceb1e6dfc9e8
tarikk <tarikelyatim2006@gmail.com>
2026-04-13 20:46:29 +0200

    feature:la registration et la deconnexion d utilisateur

--- foodswapp/app/Http/Controllers/Authcontroller.php
+++ foodswapp/app/Http/Controllers/Authcontroller.php
@@ -18,4 +18,29 @@ class Authcontroller extends Controller
             return to_route('Menu.index');
         }
     }
+
+
+    public function register(RegisterRequest $request)
+    {
+        if (User::exists()) {
+            # code...
+            $user=User::create($request->validated());
+            $user->role='admin';
+            $user->save();
+        }else{
+            $user=User::create($request->validated());
+            $user->role='client';
+            $user->save();
+        }
+        
+        return to_route('login');
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout(); 
+        $request->session()->invalidate(); 
+        $request->session()->regenerateToken(); 
+        return to_route('login'); 
+    }
 }
